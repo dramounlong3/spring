3f91069eea370a2064b22c1025b093ea22939be9
BANK\120791 <dramounlong3@sinopac.com>
2023-08-25 21:11:40 +0800

    [chroes] unit testing

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -1,9 +1,9 @@
-
+package com.example.demo;
 
 public class Calculator {
 
     public int add (int x, int y) {
-        int result = x + y;
+        return x + y;
     }
 
     public static void main(String[] args) {
---
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(99, 1);
+        assertEquals(100, result);
+    }
+}
